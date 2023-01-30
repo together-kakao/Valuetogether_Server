030408bb91702fb3dd3ca63d88080708ca976160
hyeonwook98 <hyeonwook98@naver.com>
2023-01-30 17:00:46 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/project/valuetogether/ValuetogetherApplication.java
@@ -0,0 +1,13 @@
+package project.valuetogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValuetogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValuetogetherApplication.class, args);
+	}
+
+}
